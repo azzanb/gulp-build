fed2fb45ea51ef27b17c36ce0f1c0a82fb79d64b
Azzan Braxton <azzanbraxton@gmail.com>
2017-08-27 22:21:13 -0400

    Updated

--- gulpfile.js
+++ gulpfile.js
@@ -20,44 +20,55 @@ const gulp = require('gulp'),
 // 			.pipe(gulp.dest('./css/'));
 // });
 
-gulp.task('styles', () =>{
-	return gulp.src('sass/**/*.scss')
-			.pipe(sass())
-			.pipe(map.init())
-			.pipe(useref()) //concats 
-			.pipe(iff('*.js', csso()))
-			.pipe(rename('all.min.css'))
-			.pipe(map.write('./'))
-			.pipe(gulp.dest('dist/styles'));
-});
-
-gulp.task('images', () => {
-	return gulp.src('images/*.+(png|jpg)')
-			.pipe(cache(imagemin()))
-			.pipe(gulp.dest('dist/content'))
-});
-
-gulp.task('scripts', () => {
+gulp.task('htmlScripts', ['styles'], () => {
 	return gulp.src('index.html')
-			.pipe(useref()) //concats 
-			.pipe(iff('*.js', uglify()))
-			.pipe(map.init())
+		.pipe(useref())
+		.pipe((iff('*.js', uglify()))) 			//TASK 3
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('scripts', ['htmlScripts'], () => {
+	return gulp.src('dist/scripts/*.js')
+			.pipe(map.init())					//TASK 4
 			.pipe(rename('all.min.js')) 
 			.pipe(map.write('./'))
 			.pipe(gulp.dest('dist/scripts'));
 });
 
-gulp.task('clean', () => {
-	return del.sync('dist');
+gulp.task('styles', ['htmlStyles'], () => {
+	return gulp.src('index.html')
+		.pipe(map.init())
+		.pipe(useref())                 		//TASK 2
+		.pipe(iff('*.css', csso()))
+		.pipe(rename('all.min.css'))
+		.pipe(map.write('./'))
+		.pipe(gulp.dest('dist/styles'));
 });
 
-gulp.task('connect', () => {
-	return connect.server({ port: 3000 });
+gulp.task('htmlStyles', () =>{
+	return gulp.src('sass/**/*.scss')  		    //TASK 1
+			.pipe(sass())
+			.pipe(gulp.dest('css'));
 });
+
+gulp.task('images', () => {
+	return gulp.src('images/*.+(png|jpg)')
+			.pipe(cache(imagemin()))			//TASK WHENEVER 
+			.pipe(gulp.dest('dist/content'))
+});
+
+gulp.task('clean', () => {
+	return del.sync(['dist', 'css']);
+});
+
 
 gulp.task('build', (callback) => {
 	runSequence('clean', ['scripts', 'images', 'styles'], callback);
 	gulp.src('icons/**/*').pipe(gulp.dest('dist/icons'));
+});
+
+gulp.task('connect', () => {
+	return connect.server({ port: 3000 });
 });
 
 gulp.task('default', (callback) => {
